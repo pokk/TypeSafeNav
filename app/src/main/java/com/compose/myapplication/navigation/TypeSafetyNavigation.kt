4ef7de9b93598f8de2aca55e4348a9c91437d860
Jieyi <pokkbaby@gmail.com>
2024-06-18 12:52:53 +0900

    fix(nav): remember backStackEntry for parentEntry retrieval
    use androidx.compose.runtime.remember for storing backStackEntry. update agp version in libs.versions.toml.

--- app/src/main/java/com/compose/myapplication/navigation/TypeSafetyNavigation.kt
+++ app/src/main/java/com/compose/myapplication/navigation/TypeSafetyNavigation.kt
@@ -3,6 +3,7 @@ package com.compose.myapplication.navigation
 import android.annotation.SuppressLint
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
@@ -88,7 +89,7 @@ fun TypeSafetyNavigation(modifier: Modifier = Modifier) {
             startDestination = Match,
         ) {
             composable<Match> { backStackEntry ->
-                val parentEntry = navController.getBackStackEntry(Game)
+                val parentEntry = remember(backStackEntry) { navController.getBackStackEntry(Game) }
                 val mainVM = hiltViewModel<MainViewModel>(parentEntry)
                 val vm = hiltViewModel<MatchViewModel>()
                 val secondViewModel = hiltViewModel<SecondViewModel>()
@@ -99,7 +100,7 @@ fun TypeSafetyNavigation(modifier: Modifier = Modifier) {
             }
 
             composable<InGame> { backStackEntry ->
-                val parentEntry = navController.getBackStackEntry(Game)
+                val parentEntry = remember(backStackEntry) { navController.getBackStackEntry(Game) }
                 val mainVM = hiltViewModel<MainViewModel>(parentEntry)
                 val vm = hiltViewModel<InGameViewModel>()
                 InGameScreen(
@@ -109,7 +110,7 @@ fun TypeSafetyNavigation(modifier: Modifier = Modifier) {
             }
 
             composable<ResultsWinner> { backStackEntry ->
-                val parentEntry = navController.getBackStackEntry(Game)
+                val parentEntry = remember(backStackEntry) { navController.getBackStackEntry(Game) }
                 val mainVM = hiltViewModel<MainViewModel>(parentEntry)
                 ResultWinnerScreen(
                     modifier = Modifier,
